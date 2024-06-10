7c1c87b8f2b56f10207a52e1dba75bd54398d19e
MstfaZahr <61924314+MstfaZahr@users.noreply.github.com>
2024-06-11 00:49:08 +0300

    Add files via upload

--- app/assets/app.js
+++ app/assets/app.js
@@ -1,11 +1,40 @@
-const darkIcon = document.querySelector("#dark")
-const body = document.querySelector("body")
-// const darkBody = darkIcon.querySelectorAll(".dark-body")
+// check for saved 'mode' in localStorage
+let mode = localStorage.getItem('mode');
 
-darkIcon.addEventListener("click", () => {
-   body.classList.toggle("dark")
-   body.classList.toggle("light")
-   // for (part of darkBody) {
-   //    part.classList.toggle("dark-enabled")
-   // }
-})
+const modeToggle = document.querySelector('#dark');
+
+const enableDarkMode = () => {
+   // 1. Add the class to the body
+   document.body.classList.add('dark');
+   document.body.classList.remove('light');
+   // 2. Update mode in localStorage
+   localStorage.setItem('mode', 'dark');
+}
+
+const disableDarkMode = () => {
+   // 1. Remove the class from the body
+   document.body.classList.add('light');
+   document.body.classList.remove('dark');
+   // 2. Update mode in localStorage 
+   localStorage.setItem('mode', "light");
+}
+
+// If the user already visited and enabled mode
+// start things off with it on
+if (mode === 'dark') {
+   enableDarkMode();
+}
+
+// When someone clicks the button
+modeToggle.addEventListener('click', () => {
+   // get their mode setting
+   mode = localStorage.getItem('mode');
+
+   // if it not current enabled, enable it
+   if (mode !== 'dark') {
+      enableDarkMode();
+      // if it has been enabled, turn it off  
+   } else {
+      disableDarkMode();
+   }
+});
